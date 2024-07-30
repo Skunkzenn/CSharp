3d387164ce24eed398103c66534560a444529f3f
skunkkzenn <a18586@alunos.ipca.pt>
2024-07-30 12:34:16 +0100

    Alteração exercícios resolvidos

---
+++ Exercicios_Resolvidos/VetorExercise/VetorExercise/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+
+namespace VetorExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Estudante[] aluno = new Estudante[10];
+            
+            Console.Write("Quantos quartos serão alugados? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Aluguel #{i + 1}");
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("E-mail: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+                aluno[quarto] = new Estudante(nome, email);
+                Console.WriteLine();
+            }
+
+
+            Console.WriteLine("Quartos ocupados: ");
+            for (int i = 0; i < 10; i++)
+            {
+                if(aluno[i] != null)
+                {
+                    Console.WriteLine($"{i}: {aluno[i]}");
+                }
+            }
+
+        }
+    }
+}
