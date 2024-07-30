1345fb665cbc013028dc3731090069b52f0f1817
skunkkzenn <a18586@alunos.ipca.pt>
2024-07-30 12:42:29 +0100

    adicionar conversão de string para enumeração

--- C_Sharp/Cap_8_Enumeracoes&Composicao/Categorias-de-classes.cs
+++ C_Sharp/Cap_8_Enumeracoes&Composicao/Categorias-de-classes.cs
@@ -1,9 +1,12 @@
-/* Categorias de classes
+/* 
+ * 
+ * Categorias de classes
 	• Em um sistema orientado a objetos, de modo geral "tudo" é objeto.
 	• Por questões de design tais como organização, flexibilidade, reuso, delegação, etc., há várias categorias de classes:
 
-Controllers Entities
-Services
-Views
-Repositories */
+	Controllers Entities
+	Services
+	Views
+	Repositories 
+ */
 
---
+++ C_Sharp/Cap_8_Enumeracoes&Composicao/Conversao_Enum_and_String.cs
@@ -0,0 +1,21 @@
+/* 
+ * 
+
+AVISO IMPORTANTE SOBRE CONVERSÃO DE STRING
+
+No próximo vídeo vamos aprender sobre ENUMERAÇÕES.
+
+Na aula fazemos a conversão de string para enumeração assim:
+
+OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+Mas, dependendo da versão do compilador C#, isso pode dar problema. Se for o caso, por favor tente deste jeito:
+
+OrderStatus os = (OrderStatus)Enum.Parse(typeof(OrderStatus), "Delivered");
+Se mesmo assim ainda tiver dando erro, há ainda uma terceira forma:
+
+OrderStatus os;
+Enum.TryParse("Delivered", out os);
+Atenciosamente.
+
+*/
+
