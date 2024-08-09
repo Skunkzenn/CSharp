7540bafaa7bce4c895572a2b100980f29ef58259
Skunkzenn <victor.destefani@outlook.com>
2024-08-09 12:51:40 +0100

    atualização

---
+++ Exercicios_Resolvidos/Interfaces/Solucao_com_Interface/Interfaces_List/Interfaces_List/Services/PaypalService.cs
@@ -0,0 +1,19 @@
+﻿namespace Interfaces_List.Services
+{
+    class PaypalService : IOnlinePaymentService
+    {
+        private const double SimpleTax = 0.01;
+        private const double Tax = 0.02;
+
+
+        public double Interest(double amount, int months)
+        {
+            return amount * SimpleTax * months;
+        }
+
+        public double PaymentFee(double amount)
+        {
+            return amount * Tax;
+        }
+    }
+}
