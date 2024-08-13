38f16bac8b2cbe5ce218e2dabde63135015f0beb
Skunkzenn <victor.destefani@outlook.com>
2024-08-13 16:26:24 +0100

    exercicio de generics e sets inserido

---
+++ Exercicios_Resolvidos/Generics_Set/Generics_Set_Exercicio_Class/Generics_Set_Exercicio/Entities/Student.cs
@@ -0,0 +1,29 @@
+﻿namespace Generics_Set_Exercicio.Entities
+{
+    class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+
+
+        public Student(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if(!(obj is Student)) return false;
+            Student other = (Student)obj; // Assim,se for gerado erro, vai ser possível tratar a excessão
+            //Student other2 = other as Student; Dessa forma ele apenas vai retornar null se der error
+            return Id.Equals(other.Id);
+        }
+    }
+}
---
+++ Exercicios_Resolvidos/Generics_Set/Generics_Set_Exercicio_Class/Generics_Set_Exercicio/Program.cs
@@ -0,0 +1,47 @@
+﻿using Generics_Set_Exercicio.Entities;
+
+namespace Generics_Set_Exercicio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<Student> students = new HashSet<Student>();
+
+            Console.Write("How many students for course A? ");
+            int numA = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numA; i++)
+            {
+                Console.Write("Id: ");
+                int idInsert = int.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                students.Add(new Student(idInsert, name));
+            }
+
+            Console.Write("How many students for course B? ");
+            numA = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numA; i++)
+            {
+                Console.Write("Id: ");
+                int idInsert = int.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                students.Add(new Student(idInsert, name));
+            }
+
+            Console.Write("How many students for course C? ");
+            numA = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numA; i++)
+            {
+                Console.Write("Id: ");
+                int idInsert = int.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                students.Add(new Student(idInsert, name));
+            }
+
+            Console.WriteLine(students.Count()); 
+        }
+    }
+}
